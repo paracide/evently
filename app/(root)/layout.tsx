662e59bc4b7e0bfe9566d930ac1fbf81443d4a53
Arthur <paracidex@gmail.com>
2024-01-27 17:35:17 -0600

    asset

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,6 @@
 import type {Metadata} from 'next'
-import {Poppins} from 'next/font/google'
-import './globals.css'
 
-const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700'],
-    variable: '--font-poppins',
-})
+
 
 export const metadata: Metadata = {
     title: 'Evently',
@@ -23,7 +17,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={poppins.variable}>{children}</body>
+            <body>{children}</body>
         </html>
     )
 }
