8790c63bf59e16545d1f8e032477858e030cd8cd
Arthur <paracidex@gmail.com>
2024-01-28 23:50:16 -0600

    user actions

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model, models } from "mongoose";
+import {Document, model, models, Schema} from "mongoose";
 
 export interface IEvent extends Document {
     _id: string;
@@ -17,18 +17,18 @@ export interface IEvent extends Document {
 }
 
 const EventSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String },
-    location: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    imageUrl: { type: String, required: true },
-    startDateTime: { type: Date, default: Date.now },
-    endDateTime: { type: Date, default: Date.now },
-    price: { type: String },
-    isFree: { type: Boolean, default: false },
-    url: { type: String },
-    category: { type: Schema.Types.ObjectId, ref: 'Category' },
-    organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+    title: {type: String, required: true},
+    description: {type: String},
+    location: {type: String},
+    createdAt: {type: Date, default: Date.now},
+    imageUrl: {type: String, required: true},
+    startDateTime: {type: Date, default: Date.now},
+    endDateTime: {type: Date, default: Date.now},
+    price: {type: String},
+    isFree: {type: Boolean, default: false},
+    url: {type: String},
+    category: {type: Schema.Types.ObjectId, ref: 'Category'},
+    organizer: {type: Schema.Types.ObjectId, ref: 'User'},
 })
 
 const Event = models.Event || model('Event', EventSchema);
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,12 +1,22 @@
-import { Schema, model, models } from "mongoose";
+import {model, models, Schema} from "mongoose";
+
+export type UserDto = Document& {
+    _id: string,
+    clerkId: string;
+    email: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+    photo: string;
+};
 
 const UserSchema = new Schema({
-    clerkId: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: {type: String, required: true },
-    photo: { type: String, required: true },
+    clerkId: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    photo: {type: String, required: true},
 })
 
 const User = models.User || model('User', UserSchema);
